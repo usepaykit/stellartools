e201c6dcbc3923acac31296ab2d3008fc13a3759
princeajuzie7 <princeajuzie1@gmail.com>
2025-12-22 22:12:08 +0100

    datatable skeleton abstraction

--- components/data-table.tsx
+++ components/data-table.tsx
@@ -3,7 +3,6 @@
 import * as React from "react";
 
 import { Checkbox } from "@/components/ui/checkbox";
-import { Skeleton } from "@/components/ui/skeleton";
 import {
   Table,
   TableBody,
@@ -14,6 +13,7 @@ import {
 } from "@/components/ui/table";
 import { MixinProps, splitProps } from "@/lib/mixin";
 import { cn } from "@/lib/utils";
+import { DataTableSkeleton } from "./data-table-skeleton";
 import {
   ColumnDef,
   RowSelectionState,
@@ -184,82 +184,14 @@ export function DataTable<TData, TValue>({
 
   if (isLoading) {
     return (
-      <div className="space-y-4">
-        <div className="rounded-md border">
-          <Table {...rest}>
-            <TableHeader>
-              <TableRow {...row}>
-                {enableBulkSelect && (
-                  <TableHead style={{ width: 40 }}>
-                    <Skeleton className="h-4 w-4" />
-                  </TableHead>
-                )}
-                {columns.map((column, index) => (
-                  <TableHead
-                    key={column.id || `col-${index}`}
-                    style={{
-                      width:
-                        (column.size as number) !== 150
-                          ? (column.size as number)
-                          : undefined,
-                    }}
-                  >
-                    <Skeleton className="h-4 w-24" />
-                  </TableHead>
-                ))}
-                {actions && actions.length > 0 && (
-                  <TableHead style={{ width: 50 }}>
-                    <div />
-                  </TableHead>
-                )}
-              </TableRow>
-            </TableHeader>
-            <TableBody {...body}>
-              {Array.from({ length: skeletonRowCount }).map((_, rowIndex) => (
-                <TableRow key={`skeleton-row-${rowIndex}`} {...row}>
-                  {enableBulkSelect && (
-                    <TableCell>
-                      <Skeleton className="h-4 w-4" />
-                    </TableCell>
-                  )}
-                  {columns.map((column, colIndex) => (
-                    <TableCell
-                      key={column.id || `skeleton-cell-${rowIndex}-${colIndex}`}
-                      {...cell}
-                    >
-                      <Skeleton
-                        className="h-4"
-                        style={{
-                          width: `${Math.floor(Math.random() * 40) + 60}%`,
-                        }}
-                      />
-                    </TableCell>
-                  ))}
-                  {actions && actions.length > 0 && (
-                    <TableCell>
-                      <div className="flex justify-end">
-                        <Skeleton className="h-8 w-8 rounded-md" />
-                      </div>
-                    </TableCell>
-                  )}
-                </TableRow>
-              ))}
-            </TableBody>
-          </Table>
-        </div>
-
-        <div className="flex items-center justify-end space-x-2 py-4">
-          <div className="text-muted-foreground flex-1 text-sm">
-            {enableBulkSelect && (
-              <Skeleton className="h-4 w-32" />
-            )}
-          </div>
-          <div className="space-x-2">
-            <Skeleton className="h-9 w-20 rounded border" />
-            <Skeleton className="h-9 w-16 rounded border" />
-          </div>
-        </div>
-      </div>
+      <DataTableSkeleton
+        columns={columns}
+        enableBulkSelect={enableBulkSelect}
+        actions={actions}
+        skeletonRowCount={skeletonRowCount}
+        {...rest}
+        {...{ row, body, cell }}
+      />
     );
   }
 
